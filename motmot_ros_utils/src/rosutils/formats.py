d50055d451c908327a48f7fc6d43ade6dbc95747
Andrew Straw <strawman@astraw.com>
2012-08-03 14:52:14 +0200

    clean source formatting

--- motmot_ros_utils/src/rosutils/formats.py
+++ motmot_ros_utils/src/rosutils/formats.py
@@ -8,19 +8,23 @@ def camera_calibration_yaml_to_radfile(yamlpath, radpath, lossy_ok=False):
         y = yaml.load(yf)
 
         K = y['camera_matrix']['data']
-        Knp = np.array(K); Knp.shape = y['camera_matrix']['rows'],y['camera_matrix']['cols']
+        Knp = np.array(K)
+        Knp.shape = y['camera_matrix']['rows'],y['camera_matrix']['cols']
 
         P = y['projection_matrix']['data']
-        P = np.array(P); P.shape = y['projection_matrix']['rows'],y['projection_matrix']['cols']
+        P = np.array(P)
+        P.shape = y['projection_matrix']['rows'],y['projection_matrix']['cols']
         assert np.allclose(P[:,3], np.zeros((3,)))
 
         if not lossy_ok and not np.allclose(Knp,P):
-            raise ValueError('cannot do lossless conversion to MultiCamSelfCal .rad file (camera matrix and projection matrix differ)')
+            raise ValueError('cannot do lossless conversion to MultiCamSelfCal '
+                             '.rad file (matrices differ)')
         assert y['distortion_model']=='plumb_bob'
         dist = y['distortion_coefficients']['data']
         assert len(dist)==5
         if dist[4]!=0.0:
-            raise ValueError('cannot do lossless conversion to MultiCamSelfCal .rad file (dist. coeff. k3 was %f)' % dist[4])
+            raise ValueError('cannot do lossless conversion to MultiCamSelfCal '
+                             '.rad file (dist. coeff. k3 was %f)' % dist[4])
         with open(radpath,'w') as bf:
             for row in range(3):
                 for col in range(3):
@@ -31,5 +35,3 @@ def camera_calibration_yaml_to_radfile(yamlpath, radpath, lossy_ok=False):
             for i in range(4):
                 bf.write("kc%d = %f\n" % (i+1,dist[i]))
             bf.write("\n")
-
-
